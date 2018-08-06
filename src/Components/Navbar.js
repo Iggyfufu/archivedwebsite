aca7c877b203ea8d649b57b4cc441b2571ad2788
Iggyfufu <kennethzhu07@gmail.com>
2018-08-06 14:52:13 -0400

    Feat(Navbar Contents)

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 const Navbar = () =>
   (
     <div>
+      <h1>Kenneth Zhu</h1>
+      <h2>Full Stack Developer</h2>
       <div className="nav">
         <Link to="about">About</Link>
         <Link to="work">Work</Link>
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Sidebar = () => (
   <div>
-    <img/> {/*profile image*/}
+    <img /> {/* profile image */}
     <h3>Contact</h3>
-    {/*contact info*/}
+    {/* contact info */}
   </div>
-)
+);
+
